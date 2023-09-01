f31c02ea355d1a2ca918f10cbe4ba57ec160a4c4
ramorokaneo <114235812+ramorokaneo@users.noreply.github.com>
2023-09-01 22:34:20 +0200

    Create TodoItem.js

---
+++ TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const TodoItem = ({ text }) => {
+  return (
+    <View>
+      <Text>{Text}</Text>
+    </View>
+  );
+};
+
+export default TodoItem;
